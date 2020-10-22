afcc5ccc4b3698ecc176737390a5404096bc7ade
LouisRavillon <LouisRavillon>
2020-10-22 09:48:46 +0200

    Scrap article books, multiple pages, removed xpath

--- Scraping_cnrs.py
+++ Scraping_cnrs.py
@@ -5,6 +5,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
+
 from bs4 import BeautifulSoup 
 import requests
 
@@ -18,30 +20,31 @@ url = "https://bib.cnrs.fr/#"
 ID = "18CHIMFR3417"
 mdp = "MSSLPT"
 ressource_type = "book"
-keywords = "agriculture technology"
+keywords = "technologie"
 
 driver.get(url)
 
 # Authentification
-connexion = driver.find_element_by_xpath('/html/body/header/div[2]/div[1]/div/div/button').click()
-via_code = driver.find_element_by_xpath('/html/body/div[4]/div[2]/div/div/div[2]/button[2]').click()
+connexion = driver.find_element_by_class_name("login-button.btn.btn-link").click()
+via_code = driver.find_element_by_class_name("inist-button.btn.btn-primary.btn-block").click()
 
 # Put a wait time because the window hasn't poped up yet
 driver.implicitly_wait(5)
-login = driver.find_element_by_xpath('/html/body/div[4]/div[2]/div/div/div[2]/div/div/div/input[1]')
+login = driver.find_element_by_class_name('username.form-control')
 login.send_keys(ID)
-password = driver.find_element_by_xpath('/html/body/div[4]/div[2]/div/div/div[2]/div/div/div/input[2]')
+password = driver.find_element_by_class_name('password.form-control')
 password.send_keys(mdp)
-connect = driver.find_element_by_xpath('/html/body/div[4]/div[2]/div/div/div[2]/div/div/div/button').click()
+connect = driver.find_element_by_class_name('fetch-button.api.btn.btn-primary.btn-block').click()
+driver.implicitly_wait(10)
 
 # Select if you want to search for books or articles
-if ressource_type=="article":
-    article = driver.find_element_by_xpath('/html/body/div[1]/div[1]/div/div/nav/div/ul/li[1]').click()
-    search = driver.find_element_by_xpath('/html/body/div[1]/div[1]/div/div/div/div[1]/div/div[1]/span/div/div[1]/span/div/input')
+if ressource_type=="article":  
+    article = driver.find_element_by_class_name('nav-item.nav-article.active').click()
+    search = driver.find_element_by_class_name('form-control')
 elif ressource_type=="book":
-    book = driver.find_element_by_xpath('/html/body/div[1]/div[1]/div/div/nav/div/ul/li[2]/a').click()
-    search = driver.find_element_by_xpath('/html/body/div[1]/div[1]/div/div/div/div[1]/div/div[1]/span/div/input')
-
+    book = driver.find_element_by_class_name('nav-item.nav-publication').click()
+    search = driver.find_element_by_class_name('form-control')
+    
 # Search for ressources using keywords
 search.send_keys(keywords)
 search.send_keys(Keys.ENTER)
@@ -49,21 +52,48 @@ search.send_keys(Keys.ENTER)
 # this is just to ensure that the page is loaded 
 driver.implicitly_wait(10)
 
-# Take to driver directly to the search-results path
-search_result = driver.find_element_by_xpath('/html/body/div[1]/div[1]/div/div/div/span/div/div[2]')
+# Number of links extracted
+n_links = 0
 
-# It renders the JS code and stores all of the information in static HTML code
-html = driver.page_source
+# Set current page
+current_page = 1
 
-# Now, we can simply apply bs4 to html variable 
-soup = BeautifulSoup(html, "html.parser")
+while True:
+    # Take to driver directly to the search-results path
+    search_result = driver.find_element_by_class_name('search-result')
 
-# Extract links
-if ressource_type=="article": ressources_links = soup.select("div.record.record-article [href]")
-elif ressource_type=="book": ressources_links = soup.select("div.record.record-publication [href]")
-links = []
-for ressource in ressources_links : 
-    links.append(ressource['href'])
-    print(ressource['href'])
-  
+    try:
+        record_list = driver.find_element_by_class_name("record_list")
+    except NoSuchElementException:
+        print('No result found...')
+        break
+
+    # It renders the JS code and stores all of the information in static HTML code
+    html = driver.page_source
+
+    # Now, we can simply apply bs4 to html variable 
+    soup = BeautifulSoup(html, "html.parser")
+
+    # Condition to stop (if the next page is the same)
+    page = int(soup.select("span.current.page")[0].text)
+    if (page != current_page):
+        end_message = "Last page parsed!"
+        print(end_message)
+        break
+
+    # Extract links
+    if ressource_type=="article": ressources_links = soup.select("div.record.record-article [href]")
+    elif ressource_type=="book": ressources_links = soup.select("div.record.record-publication [href]")
+    links = []
+    for ressource in ressources_links : 
+        links.append(ressource['href'])
+        print(ressource['href'])
+    n_links += len(ressources_links)
+
+    # Next page
+    pagination = driver.find_element_by_class_name("pagination")
+    next_page = driver.find_element_by_class_name("next").click()
+    current_page+=1
+
+print('Number of links extracted : {}'.format(n_links))
 driver.quit() # closing the webdriver 
