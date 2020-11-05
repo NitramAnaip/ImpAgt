65a3c13d8625e40b790345fb12f493d9b58d9ada
LouisRavillon <LouisRavillon>
2020-11-05 15:06:39 +0100

    preprocessing done

---
+++ analysis_dict.py
@@ -0,0 +1,100 @@
+import json
+import numpy as np
+
+from nltk import FreqDist
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer, WordNetLemmatizer
+from collections import Counter
+from num2words import num2words
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+
+parsed_sites = 'abs_dict.json'
+
+print("Started Reading JSON file")
+with open(parsed_sites, "r") as read_file:
+    developer = json.load(read_file)
+    print("Decoded JSON Data From File")
+
+corpus = developer['abstracts']
+
+# Preprocessing
+
+def convert_lower_case(data):
+    return np.char.lower(data)
+
+def remove_punctuation(data):
+    symbols = "!\"#$%&()*+-./:;<=>?@[\]^_`{|}~\n"
+    for i in range(len(symbols)):
+        data = np.char.replace(data, symbols[i], ' ')
+        data = np.char.replace(data, "  ", " ")
+    data = np.char.replace(data, ',', '')
+    return data
+
+def remove_apostrophe(data):
+    return np.char.replace(data, "'", "")
+
+def stemming(data):
+    stemmer= PorterStemmer()
+    tokens = word_tokenize(str(data))
+    new_text = ""
+    for w in tokens:
+        new_text = new_text + " " + stemmer.stem(w)
+    return new_text
+
+def lemming(data):
+    lemmatizer = WordNetLemmatizer() 
+    tokens = word_tokenize(str(data))
+    new_text = ""
+    for w in tokens:
+        new_text = new_text + " " + lemmatizer.lemmatize(w)
+    return new_text
+
+def convert_numbers(data):
+    tokens = word_tokenize(str(data))
+    new_text = ""
+    for w in tokens:
+        try:
+            w = num2words(int(w))
+        except:
+            a = 0
+        new_text = new_text + " " + w
+    new_text = np.char.replace(new_text, "-", " ")
+    return new_text
+
+def remove_stop_words(data):
+    stop_words = stopwords.words('english')
+    words = word_tokenize(str(data))
+    new_text = ""
+    for w in words:
+        if w not in stop_words and len(w) > 1:
+            new_text = new_text + " " + w
+    return new_text
+
+
+
+def preprocess(data):
+    data = convert_lower_case(data)
+    data = remove_punctuation(data)
+    data = remove_apostrophe(data)
+    data = remove_stop_words(data)
+    data = convert_numbers(data)
+    data = stemming(data)
+    data = lemming(data)
+    return data
+
+
+# Preprocessing
+preprocess_abstract = []
+for abstract in corpus:
+    preprocess_abstract.append(preprocess(abstract))
+
+# Word counts
+words = word_tokenize(str(preprocess_abstract[3]))
+fd = FreqDist(words)
+fd.plot(20)
+
