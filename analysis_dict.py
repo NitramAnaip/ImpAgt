b8ae64c43e58036a4cdf78725ae654d666d67de9
NitramAnaip <martin.piana@eleves.enpc.fr>
2020-11-12 10:39:28 +0100

    Still working on LDA

--- analysis_dict.py
+++ analysis_dict.py
@@ -3,7 +3,6 @@
 
 import json
 import numpy as np
-import gensim
 
 #import nltk
 from nltk import FreqDist
@@ -11,9 +10,9 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer, WordNetLemmatizer
 from collections import Counter
-from num2words import num2words
-from pprint import pprint
 
+
+# Plotting tools
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -97,68 +96,20 @@ def preprocess(data):
 # Preprocessing
 preprocess_abstract = []
 for abstract in corpus:
-    preprocess_abstract.append(word_tokenize(str(preprocess(abstract))))
+    preprocess_abstract.append(preprocess(abstract))
     #list of lists of strings (words)
 
 
 
 
-# Build the bigram and trigram models
-# Careful: it might be worth creating th bigrams etc before dtemming and lemming
-bigram = gensim.models.Phrases(preprocess_abstract) # higher threshold fewer phrases.
-trigram = gensim.models.Phrases(bigram[preprocess_abstract], threshold=100)  
 
 
-# Faster way to get a sentence clubbed as a trigram/bigram
-bigram_mod = gensim.models.phrases.Phraser(bigram)
-trigram_mod = gensim.models.phrases.Phraser(trigram)
-
-def make_bigrams(preprocess_abstract):
-    data_processed = []
-    for abstract in preprocess_abstract:
-        data_processed.append(bigram_mod[abstract])
-    return data_processed
-
-bigrams = make_bigrams(preprocess_abstract)
-
-# Create Dictionary
-id2word = gensim.corpora.Dictionary(bigrams)
-
-# Create Corpus
-texts = bigrams
-
-# Term Document Frequency
-corpus = [id2word.doc2bow(text) for text in texts]
-
-"""
-# View
-print(corpus[:1])
-# Human readable format of corpus (term-frequency)
-print([[(id2word[id], freq) for id, freq in cp] for cp in corpus[:1]])
-"""
 
 
-# Build LDA model
-lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus,
-                                           id2word=id2word,
-                                           num_topics=20, 
-                                           random_state=100,
-                                           update_every=1,
-                                           chunksize=100,
-                                           passes=10,
-                                           alpha='auto',
-                                           per_word_topics=True)
 
-
-# Print the Keyword in the 10 topics
-pprint(lda_model.print_topics())
-doc_lda = lda_model[corpus]
-
-"""
 # Word counts
 words = word_tokenize(str(preprocess_abstract[3]))
 
 fd = FreqDist(words)
 fd.plot(20)
 
-"""
