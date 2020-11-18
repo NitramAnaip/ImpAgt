e5be20ecb24f56912ea62b9796cf29b81b8215ee
LouisRavillon <LouisRavillon>
2020-11-18 16:44:03 +0100

    collect abtrasts on bib

--- journal_parsers.py
+++ journal_parsers.py
@@ -10,13 +10,40 @@ from selenium.common.exceptions import TimeoutException, NoSuchElementException
 
 from bs4 import BeautifulSoup 
 import requests
-
+import io
+import PyPDF2
 
 
 
 # Load driver (for Google Chrome)  
 chromedriver = "/usr/local/bin/chromedriver"
 os.environ["webdriver.chrome.driver"] = chromedriver
+
+
+def article_source(soup):
+    type_publication = soup.find('dt',text='Type of publication')
+    publisher = soup.find('dt',text='Publisher')
+    document_type = soup.find('dt',text='Document type')
+
+    if publisher != None:
+        source = publisher.find_next("span").text
+        return source
+
+    elif type_publication != None:
+        type_publication = type_publication.find_next("span").text
+
+        if type_publication.lower() == 'journal paper':
+            source = soup.find('dt',text='Source').find_next("span").text
+            source = source.split('Publisher:')[1].split('Country')[0]
+            return source
+
+    elif document_type != None:
+        document_type = document_type.find_next("span").text.lower()
+        if (document_type == 'working paper') | (document_type == 'report'):
+            return 'arxiv'
+
+    return 'unknown'
+
 
 def sciencedirect_abstract_parser(driver, url, abs_dict, keywords, source):
     """
@@ -55,26 +82,23 @@ def sciencedirect_abstract_parser(driver, url, abs_dict, keywords, source):
 
 
 def mdpi_abstract_parser(driver, url, abs_dict, keywords, source):
-
     try:
-        #DOI
+        html = driver.page_source
+        soup = BeautifulSoup(html, "html.parser")
 
-        doi = driver.find_element_by_class_name("bib-identity").text#get_attribute("href")
+        #DOI
+        doi = driver.find_element_by_class_name("bib-identity").text
         start = doi.find("doi.org", 0, -1) + 8
         end=doi.find("/", start, -1)
         doi = doi[start:end]
         
-        #print("doi: ", doi)
         abstract = driver.find_element_by_class_name('art-abstract.in-tab.hypothesis_container').text
         title = driver.find_element_by_class_name("title.hypothesis_container").text
-        #print("title ", title)
-        authors = driver.find_elements_by_class_name("authors")
-        
+
+        authors = soup.select("div.art-authors.hypothesis_container span.sciprofiles-link a")
         authors_list = []
         for author in authors:
             authors_list.append(author.text)
-
-        #print("authors: ", authors_list)
 
         if title not in abs_dict["titles"]:
             abs_dict["titles"].append(title)
@@ -88,7 +112,7 @@ def mdpi_abstract_parser(driver, url, abs_dict, keywords, source):
 
     return 0
 
-
+# HAL content_provider
 def HAL_abstract_parser(driver, url, abs_dict, keywords, source):
 
     try:
@@ -105,7 +129,7 @@ def HAL_abstract_parser(driver, url, abs_dict, keywords, source):
         try:
             abstract = driver.find_element_by_class_name('abstract-content').text
         except:
-            print("No abstract")
+            print("No abstract in {}".format(url))
             return 0
 
         title = driver.find_element_by_class_name("title").text
@@ -131,6 +155,126 @@ def HAL_abstract_parser(driver, url, abs_dict, keywords, source):
         print("problem with url")
 
     return 0
+
+# public library of science publisher
+def public_lib_science_abstract_parser(driver, url, abs_dict, keywords, source):
+
+    try:
+        #DOI
+        html = driver.page_source
+        soup = BeautifulSoup(html, "html.parser")
+        
+        doi = soup.find(id='artDoi')
+        if len(doi)==0: doi = 'unknown'
+        else: 
+            doi = doi.text[17:24]
+        
+        try:
+            abstract = soup.select('div.abstract.toc-section p')[0].text
+        except:
+            print("No abstract in {}".format(url))
+            return 0
+
+        title = soup.select("div.title-authors h1")[0].text
+
+        authors = soup.select("div.title-authors ul li a.author-name")
+        authors_list = []
+        for author in authors:
+            authors_list.append(author.text[1:])
+
+        if title not in abs_dict["titles"]:
+            abs_dict["titles"].append(title)
+            abs_dict["abstracts"].append(abstract)
+            abs_dict["doi"].append(doi)
+            abs_dict["authors"].append(authors_list)
+            abs_dict["keywords"].append(keywords)
+            abs_dict["sources"].append(source)
+    except:
+        print("problem with url")
+
+    return 0
+
+# springer
+def nature_abstract_parser(driver, url, abs_dict, keywords, source):
+
+    try:
+        #DOI
+        html = driver.page_source
+        soup = BeautifulSoup(html, "html.parser")
+        
+        doi = 'unknown'
+        
+        try:
+            abstract = soup.select('div.c-article-section__content p')[0].text
+        except:
+            print("No abstract in {}".format(url))
+            return 0
+
+        title = soup.select("div.c-article-header h1")[0].text
+
+        authors = soup.select("div.c-article-header li.c-author-list__item span a")
+        authors_list = []
+        for author in authors:
+            authors_list.append(author.text)
+
+        if title not in abs_dict["titles"]:
+            abs_dict["titles"].append(title)
+            abs_dict["abstracts"].append(abstract)
+            abs_dict["doi"].append(doi)
+            abs_dict["authors"].append(authors_list)
+            abs_dict["keywords"].append(keywords)
+            abs_dict["sources"].append(source)
+    except:
+        print("problem with url")
+
+    return 0
+
+
+def pdf_abstract_parser(driver, url, abs_dict, keywords, source):
+    try:
+        pdf = requests.get(url)
+        with io.BytesIO(pdf.content) as f:
+            reader = PyPDF2.PdfFileReader(f)
+            print(reader.getDocumentInfo())
+    except:
+        print("problem with url")
+
+
+
+def ieee_abstract_parser(driver, url, abs_dict, keywords, source):
+    try:
+        #DOI
+
+        doi = 'unknown'
+        html = driver.page_source
+        soup = BeautifulSoup(html, "html.parser")
+        try:
+            abstract = soup.find('meta',attrs={'property': 'og:description'})['content']
+
+        except:
+            print("No abstract in {}".format(url))
+            return 0
+
+        title = soup.select("div.document-header-title-container.col h1")[0].text
+
+        authors = soup.select("div.authors-banner-row-middle span.blue-tooltip a")
+
+        authors_list = []
+        for author in authors:
+            authors_list.append(author.text)
+
+        if title not in abs_dict["titles"]:
+            abs_dict["titles"].append(title)
+            abs_dict["abstracts"].append(abstract)
+            abs_dict["doi"].append(doi)
+            abs_dict["authors"].append(authors_list)
+            abs_dict["keywords"].append(keywords)
+            abs_dict["sources"].append(source)
+    except:
+        print("problem with url")
+
+    return 0
+
 
 """
 #BEWARE THIS IS A REINITILISATION OF OUR SAVED DATA. DO NOT USE UNLESS UTTERLY SURE OF WHAT YOU ARE DOING
---
+++ scrap_cnrs.py
@@ -0,0 +1,166 @@
+# Import bibs
+import os
+import json
+import pickle
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
+
+from journal_parsers import sciencedirect_abstract_parser, mdpi_abstract_parser,HAL_abstract_parser,public_lib_science_abstract_parser,pdf_abstract_parser,nature_abstract_parser,ieee_abstract_parser
+from journal_parsers import article_source
+from bs4 import BeautifulSoup 
+import requests
+
+# Load driver (for Google Chrome)  
+chromedriver = "/usr/local/bin/chromedriver"
+os.environ["webdriver.chrome.driver"] = chromedriver
+driver = webdriver.Chrome(chromedriver)
+driver.maximize_window()
+
+url = "https://bib.cnrs.fr/#"
+ID = "18CHIMFR3417"
+mdp = "MSSLPT"
+ressource_type = "article"
+keywords = "deep learning agriculture"
+# source = "ieee"
+#american chemical society
+# elsevier b.v., elsevier ltd, elsevier(science direct)
+# HAL
+# mdpi publishing, mpdi ag, mpdi
+# public library of science
+
+
+abstract_dict_file = "abs_dict_all.json"
+with open(abstract_dict_file) as f:
+    abs_dict = json.load(f)
+
+driver.get(url)
+
+# Authentification
+connexion = driver.find_element_by_class_name("login-button.btn.btn-link").click()
+via_code = driver.find_element_by_class_name("inist-button.btn.btn-primary.btn-block").click()
+
+# Put a wait time because the window hasn't popped up yet
+driver.implicitly_wait(5)
+login = driver.find_element_by_class_name('username.form-control')
+login.send_keys(ID)
+password = driver.find_element_by_class_name('password.form-control')
+password.send_keys(mdp)
+connect = driver.find_element_by_class_name('fetch-button.api.btn.btn-primary.btn-block').click()
+driver.implicitly_wait(10)
+
+
+# Select if you want to search for books or articles
+if ressource_type=="article":  
+    article = driver.find_element_by_class_name('nav-item.nav-article.active').click()
+    search = driver.find_element_by_class_name('form-control')
+elif ressource_type=="book":
+    book = driver.find_element_by_class_name('nav-item.nav-publication').click()
+    search = driver.find_element_by_class_name('form-control')
+    
+# Search for ressources using keywords
+search.send_keys(keywords)
+search.send_keys(Keys.ENTER)
+
+# this is just to ensure that the page is loaded 
+driver.implicitly_wait(20)
+
+#Here we will apply a few filters*******************
+
+
+xpath_publishers = "/html/body/div[1]/div[1]/div/div/div/span/div/div[1]/div/div[2]/div/span/div[6]/div[1]/button[2]/span[1]"
+xpath_languages = "/html/body/div[1]/div[1]/div/div/div/span/div/div[1]/div/div[2]/div/span/div[4]/div[1]/button[2]/span[1]"
+xpath_content_provider = "/html/body/div[1]/div[1]/div/div/div/span/div/div[1]/div/div[2]/div/span/div[7]/div[1]/button[2]/span[1]"
+
+# button = driver.find_element_by_xpath(xpath_publishers)
+# driver.execute_script("arguments[0].click();", button) #extending the language list
+# driver.implicitly_wait(5)
+    
+
+# Number of links extracted
+n_articles = 0
+
+# Set current page
+current_page = 1
+links = []
+n_page_max = 3
+
+
+while True and current_page<n_page_max:
+    # Take to driver directly to the search-results path
+    search_result = driver.find_element_by_class_name('search-result')
+    try:
+        record_list = driver.find_element_by_class_name("record_list")
+    except NoSuchElementException:
+        print('No result found...')
+        # break
+
+    # It renders the JS code and stores all of the information in static HTML code
+    notice_opener = driver.find_elements_by_class_name('notice-opener.btn.btn-link')
+
+    for notice in notice_opener:
+        driver.execute_script("arguments[0].click();", notice)
+
+    articles = driver.find_elements_by_class_name('record.record-article')
+    notice = driver.find_element_by_class_name('dl.notice-list')
+    dl_item = driver.find_elements_by_class_name("span.dl-item")
+
+    html = driver.page_source
+    soup = BeautifulSoup(html, "html.parser")
+    articles = soup.find_all('div', {'class' : 'record record-article'})
+
+    for article in articles:  
+
+        soup = article
+        try:
+            authors_list=[]
+            authors = soup.find('dt',text='Author').find_next("dd").find_all('a')
+            for author in authors:
+                authors_list.append(author.text)
+        except:
+            authors_list = 'unkown'
+        
+        try:
+            abstract = soup.find('dt',text='Abstract').find_next("span").text
+        except:
+            continue
+
+        try:
+            doi = soup.find('dt',text='DOI').find_next("span").text[:8]
+        except:
+            doi = 'unknown'
+
+        source = article_source(soup)
+
+        try:
+            title = soup.find('h4',{'class':'title'}).find('a').text.split('[')[0][2:]
+        except:
+            title = 'unknown'
+
+        if source != 'unknown':
+            if title not in abs_dict["titles"]:
+                n_articles += 1
+
+                abs_dict["titles"].append(title)
+                abs_dict["abstracts"].append(abstract)
+                abs_dict["doi"].append(doi)
+                abs_dict["authors"].append(authors_list)
+                abs_dict["keywords"].append(keywords)
+                abs_dict["sources"].append(source)
+        
+    print(' {} abstracts parsed on this page'.format(n_articles))
+
+    # Next page
+    pagination = driver.find_element_by_class_name("pagination")
+    next_page = driver.find_element_by_class_name("next").click()
+    current_page+=1
+
+print('Number of abstracts extracted : {}'.format(n_articles))
+
+with open(abstract_dict_file, 'w+') as f:
+    json.dump(abs_dict, f)
+
+driver.quit()
